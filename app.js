eedf12927ddeb0aa54cf7bbfc51a7df5cb8cae0b
Mike Ulvila <mikeulvila@me.com>
2016-02-05 08:12:15 -0600

    recieve form data, send download

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 'use strict';
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'index.html'));
@@ -17,6 +22,46 @@ const app = express();
 
 // instead of using code above to serve static webpages use .use
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/people', function (req, res) {
+  var people = [
+    {
+      first_name: "John",
+      last_name: "Doe",
+      age: 34,
+      gender: "male"
+    },
+    {
+      first_name: "Tom",
+      last_name: "Jackson",
+      age: 27,
+      gender: "male"
+    },
+    {
+      first_name: "Tracy",
+      last_name: "Smith",
+      age: 25,
+      gender: "female"
+    }
+  ];
+
+  res.json(people);
+
+});
+
+app.get('/download', function (req, res) {
+  res.download(path.join(__dirname, '/downloads/mountains.jpeg'))
+});
+
+app.get('/about', function (req, res) {
+  res.redirect('/about.html');
+});
+
+app.post('/subscribe', function (req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+  console.log(`${name} has subscribed with ${email}`);
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
